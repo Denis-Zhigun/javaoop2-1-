2e1cbe62d79dad803d6a8b85ea232291b7d624ca
Denis Zhigun <zhigundv@gmail.com>
2021-02-06 14:04:06 +0300

    Added PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,11 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public void setPosts(Post[] posts) {
+        this.posts = posts;
+    }
+}
