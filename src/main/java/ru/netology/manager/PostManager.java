d9e4d277694247208fee4e2ba3cb84da55c582cc
Denis Zhigun <zhigundv@gmail.com>
2021-02-07 22:01:57 +0300

    Added new PostManager

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,7 +5,12 @@ import ru.netology.domain.Post;
 public class PostManager {
     private Post[] posts;
 
-    public void setPosts(Post[] posts) {
-        this.posts = posts;
+    public Post[] search (int ownerId, String domain, String query, int count, int offset, int ownerOnly) {
+        return null;
     }
+
+    public void delete (int ownerId, int postId) {
+
+    }
+
 }
